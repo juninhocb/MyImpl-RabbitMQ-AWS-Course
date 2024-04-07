11874066e783b8042ce650c6f112eb86880239a2
juninhocb <juninhocb@hotmail.com>
2024-04-07 10:26:14 -0300

    first commit | feat: db conf

---
+++ src/main/java/com/carlosjr/backproposal/BackProposalApplication.java
@@ -0,0 +1,13 @@
+package com.carlosjr.backproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlosjr/backproposal/BackProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosjr.backproposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
